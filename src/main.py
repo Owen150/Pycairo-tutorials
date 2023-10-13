0cad530a8182d91215308a27da2475eb344d2a44
Michael Owen Oduor <michaeloduor50@gmail.com>
2023-10-13 17:56:46 +0300

    Setting up the Surface

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-# Main File
+import cairo
+
+# Setting up the surface
+surface = cairo.ImageSurface(cairo.FORMAT_RGB24, 600, 400)
+ctx = cairo.Context(surface)
+ctx.set_source_rgb(0.8, 0.8, 0.8)
+ctx.paint()

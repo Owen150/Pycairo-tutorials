07f4172f6dcc3bb8f43e97b136142e48a3c30fc6
Michael Owen Oduor <michaeloduor50@gmail.com>
2023-10-24 20:28:20 +0300

    Code Refactor

--- src/polygons/arrows.py
+++ src/polygons/arrows.py
@@ -1,5 +1,11 @@
 import cairo
 
+# Setting up the surface
+surface = cairo.ImageSurface(cairo.FORMAT_RGB24, 600, 400)
+ctx = cairo.Context(surface)
+ctx.set_source_rgb(0.8, 0.8, 0.8)
+ctx.paint()
+
 
 # Defining the arrow function
 def arrow(ctx, x, y, width, height, a, b):
@@ -13,24 +19,23 @@ def arrow(ctx, x, y, width, height, a, b):
     ctx.close_path()
 
 
-# Setting up the surface
-surface = cairo.ImageSurface(cairo.FORMAT_RGB24, 600, 400)
-ctx = cairo.Context(surface)
-ctx.set_source_rgb(0.8, 0.8, 0.8)
-ctx.paint()
-
 # Drawing different arrows
 ctx.set_source_rgb(0, 0, 0.5)
 arrow(ctx, 20, 20, 150, 150, 75, 50)
 ctx.fill()
+
 arrow(ctx, 220, 20, 150, 150, 50, 30)
 ctx.fill()
+
 arrow(ctx, 420, 20, 150, 150, 25, 20)
 ctx.fill()
+
 arrow(ctx, 70, 220, 75, 150, 0, 50)
 ctx.fill()
+
 arrow(ctx, 220, 220, 150, 150, 75, 0)
 ctx.fill()
+
 arrow(ctx, 420, 270, 150, 50, 100, 0)
 ctx.fill()
 

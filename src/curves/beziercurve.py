f8a077b8b7338f230e5d0200fa41ff929a0008cb
Michael Owen Oduor <michaeloduor50@gmail.com>
2023-10-25 23:39:58 +0300

    Polycurves

--- src/curves/beziercurve.py
+++ src/curves/beziercurve.py
@@ -7,14 +7,17 @@ ctx.set_source_rgb(0.8, 0.8, 0.8)
 ctx.paint()
 
 # Bezier Curve
-ctx.move_to(100, 200)
-ctx.curve_to(200, 100, 400, 310, 500, 200)
+ctx.move_to(100, 100)
+ctx.curve_to(200, 0, 400, 200, 500, 100)
+ctx.line_to(500, 300)
+ctx.curve_to(400, 400, 200, 200, 100, 300)
+ctx.close_path()
 ctx.set_source_rgb(1, 0, 0)
 ctx.set_line_width(10)
 ctx.stroke()
 
 # Save the result as a PNG image
-surface.write_to_png("bezier.png")
+surface.write_to_png("bezier_two.png")
 
 # Clean up
 ctx.show_page()

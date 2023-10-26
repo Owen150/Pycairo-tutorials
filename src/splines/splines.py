38cf6c5e016883cbf70b501e2e63475acff8b7c2
Michael Owen Oduor <michaeloduor50@gmail.com>
2023-10-26 04:03:39 +0300

    Splines

---
+++ src/splines/splines.py
@@ -0,0 +1,19 @@
+import cairo
+
+# Setting up the surface
+surface = cairo.ImageSurface(cairo.FORMAT_RGB24, 600, 400)
+ctx = cairo.Context(surface)
+ctx.set_source_rgb(0.8, 0.8, 0.8)
+ctx.paint()
+
+# Multiple Bezier Curves
+ctx.move_to(100, 120)
+ctx.curve_to(125, 150, 175, 150, 200, 110)
+ctx.curve_to(220, 175, 220, 75, 240, 50)
+ctx.set_source_rgb(1, 0, 0)
+ctx.set_line_width(5)
+ctx.stroke()
+
+surface.write_to_png("splines.png")
+
+ctx.show_page()
